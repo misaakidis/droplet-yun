77732b6e86b60309ee8c8f108c36c79439d50ecb
misaakidis <misaakidis@yahoo.gr>
2014-04-18 23:44:41 +0300

    Add license, comments, rename to droplet

--- droplet/droplet.ino
+++ droplet/droplet.ino
@@ -1,4 +1,24 @@
 /*
+
+  ********************************************************************************
+  Copyright (C) 2014 Isaakidis Marios
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  ********************************************************************************
+  
+
   ydrocare
 
   Residential water consumption monitor with Arduino Yun.
@@ -16,26 +36,20 @@
   created 17 Apr 2014
   by Marios Isaakidis
   team YdroCare
-
   https://misaakidis.github.io/blog/2014/04/water-arduino.html
-
-  This example code is in the public domain.
 
  */
 
 #include <Process.h>
 
 
-const int led = 13;        // on-board led of Arduino Yun
+const int led = 13;         // on-board led of Arduino Yun
 const int hall_input = 12;  // output of the hall effect sensor
 
-const int UID = 100;
+const int UID = 100;        // Unique Identifier for yun
 
-int peaks = 0;
-unsigned long epoch;
-char epochCharArray[25] = "";
-
-Process time;
+int peaks = 0;              // peaks since last transmission
+unsigned long epoch;        // UNIX timestamp when Arduino starts
 
 void setup() {
   // Initialize Bridge
@@ -58,12 +72,16 @@ void setup() {
 
 void loop() {
   delay(200);
+  // print virtual time
   Serial.println(epoch + (millis() / 1000));
 }
 
 
 
 unsigned long timeInEpoch() {
+  Process time;
+  char epochCharArray[25] = "";
+
   if (!time.running())  {
     time.begin("date");
     time.addParameter("+%s");
